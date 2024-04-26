fe41a10a3df0f370d4097dc6cf221f1f8481b651
fharlup <35958282+fharlup@users.noreply.github.com>
2024-04-26 14:27:41 +0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,9 @@
-﻿
 class Program
 {
     static void Main()
     {
-        StateTodo todo = new StateTodo();
-        todo.AddTask("Mengerjakan tugas 1", PengerjaanState.BlmDikerjakan);
-        todo.AddTask("Mengerjakan tugas 2", PengerjaanState.BlmDikerjakan);
-        todo.DisplayTasks();
+        StateTodo StateGuru = new StateTodo();
 
-        todo.ChangeTaskState("Mengerjakan tugas 1", PengerjaanState.Dikerjakan);
-        todo.DisplayTasks();
+        StateGuru.Jalan();
     }
 }
