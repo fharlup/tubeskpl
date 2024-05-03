3591b50d926e94ec3fdb995cea8b12388d94a79f
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-05-03 21:00:19 +0700

    Update Student Class

---
+++ ConsoleApp1/EnumofState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public enum Role
+{
+    Murid,
+    Guru
+}
+
+public enum MataPelajaran
+{
+    Matematika,
+    Fisika,
+    Kimia,
+    Biologi,
+    BahasaIndonesia,
+    BahasaInggris,
+    Sains,
+    IPA,
+    IPS
+}
+
+// State Murid
+public enum StatusPemesanan
+{
+    Pending,
+    Ongoing,
+    Done
+}
+
+// State Guru
+public enum PengerjaanState
+{
+    bersedia,
+    SudahDipesan,
+    Mengajar
+};
+
+public enum Trigger
+{
+    Pesan,
+    cancel,
+    mengajar,
+    selesai
+};
+
+public class User
+{
+    public Role Role { get; set; }
+    public string Name { get; set; }
+    public List<MataPelajaran> MataPelajaran { get; set; }
+}
+
+public class Pemesanan
+{
+    public User Guru { get; set; }
+    public User Murid { get; set; }
+    public MataPelajaran MataPelajaran { get; set; }
+    public StatusPemesanan Status { get; set; }
+}
