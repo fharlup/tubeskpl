80dbc18cae1684771c125550064e80cc71525bf5
purianagatha <purianagatha@gmail.com>
2024-05-03 20:03:12 +0700

    Menambahkan enum role, mataPelajaran, statusPemesanan

---
+++ ConsoleApp1/Automata_Enum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Role
+{
+    Murid,
+    Guru
+}
+
+public enum MataPelajaran
+{
+    Matematika,
+    Fisika,
+    Kimia,
+    Biologi,
+    BahasaIndonesia,
+    BahasaInggris,
+    Sains,
+    IPA,
+    IPS
+}
+
+public enum StatusPemesanan
+{
+    Available,
+    Booking,
+    Done
+}
+
+public class User
+{
+    public Role Role { get; set; }
+    public string Name { get; set; }
+    public List<MataPelajaran> MataPelajaran { get; set; }
+}
+
+public class Pemesanan
+{
+    public User Guru { get; set; }
+    public User Murid { get; set; }
+    public MataPelajaran MataPelajaran { get; set; }
+    public StatusPemesanan Status { get; set; }
+}
