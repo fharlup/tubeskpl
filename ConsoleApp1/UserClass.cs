8aa56e9879520b87c63511f00206815c8e21a13e
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-05-03 21:24:53 +0700

    Add Student Class attribute, Teacher Class attribute, Order Class attribute.

---
+++ ConsoleApp1/UserClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Guru
+{
+    private string namaGuru;
+    private int umur;
+    private string mpKeahlian;
+    private PengerjaanState status;
+}
+
+public class Murid
+{
+    private string namaMurid;
+    private int umur;
+    private string pendidikan;
+    //private PengerjaanState status; 
+}
+
+public class JadwalPesanan
+{
+    private string mataPelajaran;
+
+    // diganti jadi date-time
+    // private string tanggalPesanan;
+
+    private Guru guru;
+    private Murid murid;
+    private StatusPemesanan statusPesanan;
+}
