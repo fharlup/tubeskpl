72f360a7f1ec54ab81b521e0b4473226a2ff4a4a
yazidalg <yazid.al2418@gmail.com>
2024-05-06 03:27:01 +0700

    create dto folders for authentication

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace API.Controllers
+{
+	public class AuthController
+	{
+		public AuthController()
+		{
+		}
+	}
+}
+
---
+++ API/Dtos/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace API.Dtos
+{
+	public class LoginRequest
+	{
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
+
