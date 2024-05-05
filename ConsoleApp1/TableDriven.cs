a313650666b66a475489db7a45e6dfa6a22d8930
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-06 00:27:49 +0700

    Menambahkan Table konsep Driven

---
+++ ConsoleApp1/TableDriven.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TableDriven 
+    {
+        public enum MataPelajaran
+        {
+            Matematika,
+            Fisika,
+            Kimia,
+            Biologi,
+            BahasaIndonesia,
+            BahasaInggris,
+            Sains,
+            IPA,
+            IPS
+        }
+        public static readonly Dictionary<string, MataPelajaran> matpel = new Dictionary<string, MataPelajaran>
+        {
+            {"Matematika", MataPelajaran.Matematika},
+            {"Fisika",MataPelajaran.Fisika},
+            {"Kimia" ,MataPelajaran.Kimia},
+            {"Biologi",MataPelajaran.Biologi},
+            {"Bahasa Indonesia", MataPelajaran.BahasaIndonesia},
+            {"Bahasa Inggris", MataPelajaran.BahasaInggris},
+            {"Sains", MataPelajaran.Sains},
+            {"IPA", MataPelajaran.IPA},
+            {"IPS", MataPelajaran.IPS},
+        };
+        public static MataPelajaran GetMatpel (String Matapelajaran)
+        {
+            return matpel [Matapelajaran];
+        }
+    }
+   
+}
